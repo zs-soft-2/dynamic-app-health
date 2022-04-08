bc8fb5a49f1e8db2108a500f4d5c79b04444a353
zsagia <zsagia@gmail.com>
2022-04-08 12:42:11 +0200

    Add ngrx to dynamic-app

--- apps/dynamic-app/src/app/app.component.ts
+++ apps/dynamic-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'dynamic-app-health-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'dynamic-app-health-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'dynamic-app';
+	public title = 'dynamic-app';
 }
--- apps/dynamic-app/src/app/app.module.ts
+++ apps/dynamic-app/src/app/app.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		StoreModule.forRoot(
+			{},
+			{
+				metaReducers: !environment.production ? [] : [],
+				runtimeChecks: {
+					strictActionImmutability: true,
+					strictStateImmutability: true,
+				},
+			}
+		),
+		EffectsModule.forRoot([]),
+		!environment.production ? StoreDevtoolsModule.instrument() : [],
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
